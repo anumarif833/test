4023d81562d37b3159a863b05bae3ed2a11e88e2
anumarif <anumarif833@gmail.com>
2024-05-31 16:26:16 +0500

    commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//  let studentName ["aata"  , "namak" , "'dahi" , "ghee" , "oil" ]
+//      grocery.pop()
+//      grocery.push("chawal")
+
+//     grocery.shift()
+//     grocery.splice(1,2,"kala namak")
+//     grocery.splice(0.2,"kala namak")
+
+var manufactures = ["apple","samsong","motrola", "nokia"];
+document.write('<selet>')
+document.write('<option value ="'= "apple" + factures[1] +'">'+factures[0]+ '"</option>"'
+[0] +'</option>')
+document.write('/selet')
